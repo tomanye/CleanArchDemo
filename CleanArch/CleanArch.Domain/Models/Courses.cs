ab224c8d33001fdda3b221c46a933dbd89347c2a
tomanye <manyazehal@gmail.com>
2019-05-07 16:54:29 +0300

    Add Course Domain Model

---
+++ CleanArch/CleanArch.Domain/Models/Courses.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArch.Domain.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
